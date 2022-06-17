92a37038a002e16c2b2c02c2dc48afa6f4b88354
jwbrowning <jwbrowning19@gmail.com>
2022-06-17 11:02:36 -0500

    removed coming soon info

--- src/Components/follow.js
+++ src/Components/follow.js
@@ -950,7 +950,7 @@ export default function Follow() {
         <div>
             {!comingSoon ? 
                 <div>
-                <div style={{
+                {/* <div style={{
                     margin: '50px',
                 }}>
                     <h2 className='info-text'>coming soon:</h2>
@@ -958,7 +958,7 @@ export default function Follow() {
                     <h2 className='info-text3'>-live win/draw/loss chances for each game updated move by move</h2>
                     <h2 className='info-text3'>-live tournament victory chances for each player updated each round AND each move</h2>
                     <h2 className='info-text3'>-see how different results affect tournament victory chances</h2>
-                </div>
+                </div> */}
                 {apiFailed > 6 ? <div className='fail-popup'>
                     <h2 className='fail-text'>Failed to retrieve tournament data. Please try again.</h2>
                 </div> : <></>}
